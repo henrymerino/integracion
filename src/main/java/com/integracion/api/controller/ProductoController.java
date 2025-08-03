f06d5ec5ce28450801910d9ddfa856edc49d7477
henrymerino <haguilarmerino@gmail.com>
2025-08-02 20:17:55 -0500

    mensaje slack

--- src/main/java/com/integracion/api/controller/ProductoController.java
+++ src/main/java/com/integracion/api/controller/ProductoController.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
+//comentario
 @RestController
 @RequestMapping("/api/v1/productos")
 public class ProductoController {
