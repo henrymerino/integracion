283056a6dcd749e1917ec94c2878719bdcd8f9bc
henrymerino <haguilarmerino@gmail.com>
2026-05-30 15:15:05 -0500

    cambio archivo 16

--- src/main/java/com/integracion/api/model/Calculadora.java
+++ src/main/java/com/integracion/api/model/Calculadora.java
@@ -5,45 +5,11 @@ public class Calculadora {
 
     private int resultado;
 
-    public void sumar(int a, int b) {
-        resultado = a + b;
-    }
-
-    public void restar(int a, int b) {
-        resultado = a - b;
-    }
-
-    public void multiplicar(int a, int b) {
-        resultado = a * b;
-    }
-
-    public void dividir(int a, int b) {
-        if (b == 0) {
-            throw new IllegalArgumentException(
-                    "El divisor no puede ser cero");
-        }
-        resultado = a / b;
-    }
-
     public int getResultado() {
         return resultado;
     }
 
-    public void operacionLarga() {
-        for (int i = 0; i < 100; i++) {
-            procesarNivel(i);
-        }
-    }
-
-    private void procesarNivel(int i) {
-        for (int j = 0; j < 100; j++) {
-            procesarSubNivel(j);
-        }
-    }
-
-    private void procesarSubNivel(int j) {
-        for (int k = 0; k < 100; k++) {
-            // procesamiento
-        }
+    public void setResultado(int resultado) {
+        this.resultado = resultado;
     }
 }
